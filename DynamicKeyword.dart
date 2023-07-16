8c6602db699f6a574f1c618707cfb39702657a72
Pranshu Chaurasia <66831697+ChaurasiaP@users.noreply.github.com>
2023-07-16 23:14:17 +0530

    Variables in dart

---
+++ DynamicKeyword.dart
@@ -0,0 +1,10 @@
+main() {
+  dynamic n = "hello";
+  print(n);
+
+  n = 10;  // dynamic variables can store any data of any data type if assigned implicitly
+  print(n);
+
+  n = 10.45;
+  print(n);
+}
---
+++ VarKeyword.dart
@@ -0,0 +1,9 @@
+main(){
+  var x = 10;
+  print(x);
+
+  x = 30;
+  //x="hello";
+  //error will occur because var is assigned int and can be reassigned other data type
+  print(x);
+}
